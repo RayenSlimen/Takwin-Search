092291e7d638531a941b86878a75b5bda63742c5
Rayen <rayenslimen932@gmail.com>
2025-09-15 18:37:09 +0100

    master: added home page

--- takwin/urls.py
+++ takwin/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('centre_de_formation.html', views.centre_de_formation_view, name='centre_de_formation'),
+    path('', views.centre_de_formation_view, name='home'),  
+    path('centre_de_formation.html', views.centre_de_formation_view, name='centre_de_formation'),  
     path('signup.html', views.signup, name='signup'),
     path('login.html', views.login, name='login'),
     path("sessionetudiant.html", views.sessionetudiant, name="sessionetudiant" ),
